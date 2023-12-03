a61df19decad415432d89f0b7eb06342614b26eb
ValeriyChepelev <valeriychepelev@gmail.com>
2023-12-03 16:38:57 +0300

    table generator draft in main

--- main.py
+++ main.py
@@ -49,8 +49,26 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def table_gener(icd, nsd, iec_data):
+    """Creates all the required mics tables.
+    """
+    for ld in list_ld(icd):
+        print(f'Logical Device instance "{ld[0]}"')
+        for ln in list_ln(icd, ld[0]):
+            print(f'{ln[1]} Logical Node "{ln[0]}":{ln[3]} ({ln[2]})')
+
+
+def test_tab_gener():
+    icd_tree = xml_tree.parse('ICD-152-KSZ-41_200.icd')
+    icd_root = icd_tree.getroot()
+    nsd_tree = xml_tree.parse('IEC_61850-7-4_2007B.nsd')
+    nsd_root = nsd_tree.getroot()
+    iec_data = read_data('foo')
+    table_gener(icd_root, nsd_root, iec_data)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test_ass_reader()
+    test_tab_gener()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
