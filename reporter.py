524a0301b3a933a645425accfb43187a9cfd9485
ValeriyChepelev <valeriychepelev@gmail.com>
2023-12-16 16:34:39 +0300

    poc of cycling

--- reporter.py
+++ reporter.py
@@ -8,16 +8,19 @@ from docxtpl import DocxTemplate
 
 def report(template):
     doc = DocxTemplate(template)
-    ldt = [{'inst': 'LD0',
-            'name': 'device0',
-            'desc': 'description of device 0'},
-           {'inst': 'LD1',
-            'name': 'device1',
-            'desc': 'description of device 1'},
-           {'inst': 'LD2',
-            'name': 'device2',
-            'desc': 'very-very long poem of long superior description of device 2'}]
+    ldt1 = [{'inst': 'LD0',
+             'name': 'device0',
+             'desc': 'description of device 0'},
+            {'inst': 'LD1',
+             'name': 'device1',
+             'desc': 'description of device 1'},
+            {'inst': 'LD2',
+             'name': 'device2',
+             'desc': 'very-very long poem of long superior description of device 2'}]
+    ldt2 = [{'inst': 'LDA',
+             'name': 'deviceA',
+             'desc': 'description of device A'}]
     context = {'name': 'БФПО-152-КСЗ-41_200',
-               'ld_tbl': ldt}
+               'tables': [ldt1, ldt2]}
     doc.render(context)
     doc.save('generated_doc.docx')
